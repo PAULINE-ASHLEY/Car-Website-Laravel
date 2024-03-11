fc241ff9e3921b050a5034b6097a459ea3ff20cd
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2024-03-11 09:16:04 +0300

    I have created new pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,8 +16,12 @@
     </head>
     <body>
         <div  class='flex flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row h-screen'>
-            <div class="bg-blue-600 md:w-[40%] lg:w-[40%] xl:w-[40%] 2xl:w-[40%] py-10">
-
+            <div class="bg-blue-600 md:w-[40%] lg:w-[40%] xl:w-[40%] 2xl:w-[40%] py-24">
+                <h1 class="text-white text-center text-3xl font-600">Ash Car Rental</h1>
+                <div class="px-10">
+                <img src="{{ asset('/front/assets/images/background2.png') }}">
+                </div>
+                <p class="text-white text-center text-xl font-600 px-4">It is an online system through which customers can view available cars and book cars.</p>
             </div>
             <div class="md:w-[60%] lg:w-[60%] xl:w-[60%] 2xl:w-[60%] p-10">
                 {{ $slot }}
